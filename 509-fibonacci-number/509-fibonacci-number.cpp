0ccc645b1039beacd69e3d9793a61a2663fdbefb
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-07-06 11:31:40 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (9.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(vector<int>& dp,int n){
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=solve(dp,n-1)+solve(dp,n-2);
+        return dp[n];
+    }
+    int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1) return 1;
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        return solve(dp,n);
+        
+    }
+};
