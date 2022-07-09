4634daa7c694e9a5973702de825e559baefbd4d7
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-07-10 00:18:45 +0530

    Time: 316 ms (48.62%), Space: 78.4 MB (85.89%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -38,21 +38,7 @@ public:
     {
         long curr=0;
         deque<int> dq;     		
-//         for(int i=nums.size()-1;i>=0;i--)
-//         {
-//             curr=nums[i]+(dq.empty()?0:nums[dq.front()]);       
-			
-			
-//             while(!dq.empty()&&curr>nums[dq.back()])
-//                 dq.pop_back();	
-//             dq.push_back(i);              
-			
-		
-//             if(dq.front()>=i+k)
-//                 dq.pop_front();
-//             nums[i]=curr;               
-//         }
-//         return curr;  
+  
         for(int i=0;i<nums.size();i++){
             if(!dq.empty()){
                 curr = nums[i]+nums[dq.front()];
