727252f99970407bc4fc872bcdf5b6cd5b814c82
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-07-17 10:06:52 +0530

    Time: 2706 ms (5.13%), Space: 10.2 MB (83.33%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int kInversePairs(int n,int k){
+        int dp[1001][1001]={1};
+        for(int N=1;N<=n;++N)
+            for(int K=0;K<=k;++K)
+                for(int i=0;i<=min(K,N-1);++i)
+                    dp[N][K]=(dp[N][K]+dp[N-1][K-i])%1000000007;
+        return dp[n][k];
+    }
+    // int kInversePairs(int n, int k) {
+    //     if(k==0){
+    //         return 1;
+    //     }
+    //     if(dp[n][k]==0){
+    //         for(int i=0;i<=min(k,n-1);i++){
+    //             dp[n][k]=(dp[n][k]+kInversePairs(n-1,k-i))%1000000007;
+    //         }
+    //     }
+    //     return dp[n][k];
+    // }
+};
