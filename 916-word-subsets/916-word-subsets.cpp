69f162f5466c9543ebad86e96052dacd7b674db4
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-07-30 13:08:14 +0530

    Time: 987 ms (13.26%), Space: 154.3 MB (23.37%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+         vector<string> ans;
+         vector<unordered_map<char,int>> v;
+        set<string> st;
+        for(int j=0;j<words2.size();j++){
+            st.insert(words2[j]);
+        }
+        for(auto x:st){
+            unordered_map<char,int> mpp;
+            for(int k=0;k < x.size();k++){
+                mpp[x[k]]++;
+            }
+            v.push_back(mpp);
+        }
+        unordered_map<char,int> hehe;
+        for(int i=0;i<v.size();i++){
+            for(auto x:v[i]){
+                if(hehe.find(x.first)==hehe.end()){
+                    hehe[x.first]=x.second;
+                }
+                else{
+                    if(x.second > hehe[x.first]){
+                        hehe[x.first]=x.second;
+                    }
+                }
+            }
+        }
+        
+        // for(int j=0;j<words2.size();j++){
+        //     unordered_map<char,int> mpp;
+        //     for(int k=0;k<words2[j].size();k++){
+        //         mpp[words2[j][k]]++;
+        //     }
+        //     v.push_back(mpp);
+        // }
+        for(int i=0;i<words1.size();i++){
+            bool check = true;
+            unordered_map<char,int> mpp1;
+            for(int j=0;j<words1[i].size();j++){
+                mpp1[words1[i][j]]++;
+            }
+            
+            for(auto x:hehe){
+                if(mpp1.find(x.first)==mpp1.end() || mpp1[x.first] < x.second){
+                    check = false;
+                    break;
+                }
+            }
+            if(check){
+                ans.push_back(words1[i]);
+            }
+        }
+        return ans;
+    }
+};
