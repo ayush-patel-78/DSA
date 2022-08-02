08b3faa887ebe67f7801d1f1601d5d3de30c4240
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-08-02 23:57:52 +0530

    Time: 45 ms (69.61%), Space: 13.1 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,18 +1,50 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-      priority_queue<int> pq;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix.size();j++){
-                if(pq.size()<k){
-                    pq.push(matrix[i][j]);
+    
+    // 1st approach mine
+    // int kthSmallest(vector<vector<int>>& matrix, int k) {
+    //   priority_queue<int> pq;
+    //     for(int i=0;i<matrix.size();i++){
+    //         for(int j=0;j<matrix.size();j++){
+    //             if(pq.size()<k){
+    //                 pq.push(matrix[i][j]);
+    //             }
+    //             else{
+    //                 pq.push(matrix[i][j]);
+    //                 pq.pop();
+    //             }
+    //         }
+    //     }
+    //     return pq.top();
+    // }
+    
+    // better approach nested binary search
+    
+    int kthSmallest(vector<vector<int>>& matrix,int k) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int startVal = matrix[0][0];
+        int endVal = matrix[row-1][col-1];
+        int midVal;
+        
+        while(startVal <= endVal){
+            midVal = (startVal + endVal)/2;
+            int ans = 0;
+            for(int i=0;i<row;i++){
+                int low = 0;
+                int high = col -1;
+                int m;
+                while(low <= high){
+                    m = (low + high)/2;
+                    if(matrix[i][m] <= midVal) low = m+1;
+                    else high = m-1;
                 }
-                else{
-                    pq.push(matrix[i][j]);
-                    pq.pop();
-                }
+                ans += low;  
             }
+            if(ans < k) startVal = midVal +1;
+            else endVal = midVal -1;
         }
-        return pq.top();
+        return startVal;
     }
 };
