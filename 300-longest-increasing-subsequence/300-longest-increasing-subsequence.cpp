f447ca6205f67623c5d56bfb17dcfd8d0efb6a60
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-08-08 09:51:54 +0530

    Time: 1825 ms (7.04%), Space: 292.5 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int solve(vector<int>& nums,int index,int n,int prev,vector<vector<int>>&dp){
        if(index == n) return 0;
-        if(prev!= -1 && dp[index][prev]!= -1) return dp[index][prev];
+        if(dp[index][prev+1]!= -1) return dp[index][prev+1];
        int len = solve(nums,index+1,n,prev,dp);
         
         if(prev==-1 || nums[index]>nums[prev]){
             len = max(len,solve(nums,index+1,n,index,dp)+1);
         }
-        if(prev!=-1) return dp[index][prev]=len;
-        return len;
+         return dp[index][prev+1]=len;
+        
         
     }
     int lengthOfLIS(vector<int>& nums) {
