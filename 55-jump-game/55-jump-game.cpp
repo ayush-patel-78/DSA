de734a4aa2002ce07302aede0c5ac82404fbc9ff
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-08-10 14:41:02 +0530

    Time: 2093 ms (5.00%), Space: 51.1 MB (8.08%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,38 +2,38 @@ class Solution {
 public:
     
     // my approach 
-//     bool solve(vector<int>& nums,int index,vector<int>& dp){
-//         if(index == nums.size()-1) return true;
-//         if(dp[index] != -1) return dp[index];
-//         int n = nums[index];
-//         for(int i=1;i<=n;i++){
-//          bool check  = solve(nums,index+i,dp);
-//             if(check){
-//                  dp[index]=1;
-//                 return true;  
-//             }
+    bool solve(vector<int>& nums,int index,vector<int>& dp){
+        if(index >= nums.size()-1) return true;
+        if(dp[index] != -1) return dp[index];
+        int n = nums[index];
+        for(int i=1;i<=n;i++){
+         bool check  = solve(nums,index+i,dp);
+            if(check){
+                 dp[index]=1;
+                return true;  
+            }
            
-//         }
-//         dp[index]=0;
-//         return false;
+        }
+        dp[index]=0;
+        return false;
         
-//     }
-//     bool canJump(vector<int>& nums) {
-//         vector<int> dp(nums.size()+1,-1);
-//         return solve(nums,0,dp);
-//     }
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+        return solve(nums,0,dp);
+    }
     
     // better approach 
-      bool canJump(vector<int>& nums) {
-        int i, minjump = 0;
-        for(i = nums.size()-2; i >= 0; i--){
-            minjump++;
-            if(nums[i] >= minjump)
-    minjump = 0;
-        }
-        if(minjump == 0) 
-    return true;
-        else 
-    return false;
-    }
+    //   bool canJump(vector<int>& nums) {
+    //     int i, minjump = 0;
+    //     for(i = nums.size()-2; i >= 0; i--){
+    //         minjump++;
+    //         if(nums[i] >= minjump)
+    //             minjump = 0;
+    //         }
+    //         if(minjump == 0) 
+    //             return true;
+    //         else 
+    //             return false;
+    // }
 };
