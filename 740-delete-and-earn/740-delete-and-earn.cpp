f67b1b24d40fe4dce79d7f823b312a94b4cc37c4
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-08-11 22:09:03 +0530

    Time: 26 ms (46.40%), Space: 13.8 MB (48.89%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
-    bool static compare(pair<int,int>& p1,pair<int,int>& p2){
-        return p1.first<p2.first;
-    }
-//     int solve(vector<pair<int,int>>& arr,int index,vector<int>& dp,int prev){
-//         if(index >= arr.size()) return 0;
-//         if(dp[index] != -1) return dp[index];
-//         int ans2 = 0;
-        
-//         if(arr[index].first != prev +1 && arr[index].first != prev -1){
-//             ans2 = solve(arr,index+1,dp,arr[index].first) + arr[index].second;
-           
-//         }
-//         int ans1 = solve(arr,index+1,dp,prev);
-        
-        
-//         return dp[index]=max(ans1,ans2);
-//     }
+  
+
     int solve2(vector<int>& nums,int index,vector<int>& dp)
     {
         if(index>=nums.size()){
@@ -31,22 +16,13 @@ public:
     }
     int deleteAndEarn(vector<int>& nums) {
         unordered_map<int,int> mpp;
+         int maximum = 0;
+       
         for(int i=0;i<nums.size();i++){
             mpp[nums[i]]+=nums[i];
-         
+            if(nums[i]>maximum) maximum = nums[i];
         }
-        vector<pair<int,int>> arr;
-        vector<int> points;
-        int maximum = 0;
-        for(auto x:mpp){
-            arr.push_back({x.first,x.second});
-            if(x.first > maximum) maximum = x.first;
-            
-        }
-        // sort(arr.begin(),arr.end(),compare);
-        // for(int i=0;i<arr.size();i++){
-        //     points.push_back(arr[i].second);
-        // }
+       
         vector<int> v(maximum+1);
         for(int i=0;i<=maximum;i++){
             if(mpp.find(i)!=mpp.end())
