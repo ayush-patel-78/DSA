9b93743d1167f18fb2243c4205deb1880076925f
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-08-12 11:38:16 +0530

    Time: 47 ms (49.88%), Space: 23.3 MB (12.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,17 +10,32 @@
 
 class Solution {
 public:
-    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root == NULL) return NULL;
-        if(root == p || root == q) return root;
-        TreeNode* left_subtree = solve(root->left,p,q);
-        TreeNode* right_subtree = solve(root->right,p,q);
-        if(left_subtree != NULL && right_subtree != NULL) return root;
-        else if(left_subtree != NULL) return left_subtree;
-        else if(right_subtree != NULL) return right_subtree;
-        else return NULL;
+    // My approach 
+    
+    // TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q){
+    //     if(root == NULL) return NULL;
+    //     if(root == p || root == q) return root;
+    //     TreeNode* left_subtree = solve(root->left,p,q);
+    //     TreeNode* right_subtree = solve(root->right,p,q);
+    //     if(left_subtree != NULL && right_subtree != NULL) return root;
+    //     else if(left_subtree != NULL) return left_subtree;
+    //     else if(right_subtree != NULL) return right_subtree;
+    //     else return NULL;
+    // }
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    //     return solve(root,p,q);
+    // }
+    //
+    // Better approach 
+    TreeNode* binarySearch(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==NULL) return NULL;
+        if(root->val > p->val && root->val > q->val) 
+            return binarySearch(root->left,p,q);
+        if(root->val < p->val && root->val < q->val) 
+            return binarySearch(root->right,p,q);
+        return root;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return solve(root,p,q);
+        return binarySearch(root,p,q);
     }
 };
