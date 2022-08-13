12499bb5bcf5fa54ff475b9cf1826916d5dad9cb
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-08-13 18:03:40 +0530

    Time: 1575 ms (5.05%), Space: 39.5 MB (52.31%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -5,7 +5,7 @@ public:
         
         ll maximum = 0;
         for(ll i=0;i<values.size();i++){
-            for(ll j=i+1;j<values.size() && (j-i)<=values[i];j++){
+            for(ll j=i+1;j<values.size() && (j-i)<=values[i] ;j++){
                 int curr = values[i]+values[j] -j + i ;
                 if(curr > maximum) maximum = curr;
             }
