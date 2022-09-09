7c172015f2982b75136a35824d2dbf67146f6788
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-09-10 00:27:33 +0530

    Time: 1574 ms (8.76%), Space: 125.1 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,34 +1,3 @@
-// class comparator{
-//     public:
-//     bool operator()(vector<int>const& v1,vector<int>const& v2){
-//         if(v1[1]>v2[1]) return true;
-//         else if(v1[1]==v2[1] && v1[0]>v2[0]) return true;
-//         return false;
-//     }
-// };
-// class Solution {
-// public:
-//     bool static compare(vector<int>& v1,vector<int>& v2){
-//         if(v1[0]<v2[0]) return true;
-//         else if(v1[0]==v2[0] && v1[1]>v2[1]) return true;
-//         return false;
-//     }
-//     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-//         sort(properties.begin(),properties.end(),compare);
-              
-//         int count = 0;
-//         long long max_def = INT_MIN;
-//         for(int i=properties.size()-1;i>=0;i--){
-//             if(properties[i][1]< max_def ) count++;
-//             else{
-//                 max_def = properties[i][1];
-//             }
-            
-//         }
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
     void primt(const vector<vector<int>>& prop){
@@ -62,7 +31,7 @@ public:
             }
             if(max1 > mat[i][0] && max2 > mat[i][1]){
                 ans++;
-                cout<<mat[i][0]<<" "<< mat[i][1]<<" < ";
+                // cout<<mat[i][0]<<" "<< mat[i][1]<<" < ";
             }
             else if(max2== mat[i][1]){ 
                 if(max1_ > mat[i][0] && max2_ > mat[i][1]){
