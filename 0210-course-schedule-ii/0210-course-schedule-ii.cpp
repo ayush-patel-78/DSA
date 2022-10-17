5ecc19ff842fc5fc01c811ea906aa8cdda6b33df
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-10-17 23:18:13 +0530

    Time: 44 ms (50.41%), Space: 14.9 MB (23.50%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void dfs(vector<int>& vis,int v,vector<vector<int>>& adjList,stack<int>& st){
+        vis[v]=1;
+        for(int i=0;i<adjList[v].size();i++){
+            if(!vis[adjList[v][i]]){
+                dfs(vis,adjList[v][i],adjList,st);
+            }
+        }
+        st.push(v);
+    }
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        for(auto x:prerequisites){
+            adjList[x[1]].push_back(x[0]);
+            if(x[1]==x[0]) return {};
+        }
+        bool check = true;
+        vector<int> vis(numCourses,0);
+        stack<int> st;
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                dfs(vis,i,adjList,st);
+            }
+        }
+        vector<int> topo;
+        while(!st.empty()){
+            topo.push_back(st.top());
+            st.pop();
+        }
+        vector<int> vis2(numCourses,0);
+        for(int i=topo.size()-1;i<topo.size();i--){
+            if(!vis2[topo[i]]){
+                dfs(vis2,topo[i],adjList,st);
+            }
+            else{
+                return {};
+            }
+            
+        }
+        
+        return topo;
+    }
+};
