bdb297f5919d9028e09cc8871bb49e324374a7b7
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-10-17 10:18:15 +0530

    Time: 61 ms (18.43%), Space: 13 MB (38.52%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        string s;
+        int i = n;
+        int j = m;
+      while(i>0 && j>0){
+          if(str1[i-1] == str2[j-1]){
+              s.push_back(str1[i-1]);
+              i--;
+              j--;
+          }
+          else{
+              if(dp[i-1][j] > dp[i][j-1]){
+                  i--;
+              }else j--;
+          }
+      }
+      reverse(s.begin(),s.end());
+        i = 0;
+        j = 0;
+        int k = 0;
+        string ans;
+      while(i<n || j<m){
+          if(str1[i]==str2[j] && str1[i]==s[k]){
+              ans.push_back(s[k]);
+              i++;
+              j++;
+              k++;
+          }
+          if(i<n && str1[i] != s[k]){
+              ans.push_back(str1[i]);
+              i++;
+          }
+          if(j<m && str2[j] != s[k]){
+              ans.push_back(str2[j]);
+              j++;
+          }
+      }
+      return ans;
+    }
+};
