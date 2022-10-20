a74893c3b2d7632b0224969b924853523bf547bc
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-10-20 12:52:27 +0530

    Time: 43 ms (9.90%), Space: 9.3 MB (22.14%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -12,7 +12,6 @@ public:
         mpp[1000]="M";
         string ans;
         vector<int> vec={1000,500,100,50,10,5,1};
-        vector<string> hehe;
         for(int i=0;i<vec.size();i+=2){
             string s="";
             int x = num/vec[i];
@@ -37,8 +36,6 @@ public:
             else if(x==9){
                 s = mpp[vec[i]] + mpp[vec[i-2]];
             }
-            
-            hehe.push_back(s);
             ans += s;
         }
         return ans;
