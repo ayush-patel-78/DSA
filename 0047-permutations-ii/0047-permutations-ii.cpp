b0ed14334b0a956e6efaa568fa250443955d7cf4
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-10-21 05:10:53 +0530

    Time: 3 ms (98.93%), Space: 8.6 MB (80.12%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,34 +1,67 @@
 class Solution {
 public:
-       
-    void solve(vector<int> nums,set<vector<int>>& st,int i){
-        for(int j = i;j<nums.size();j++){
-            vector<int> vec;
-            int count = 0;
-            for(int x=0;x<i;x++){
-                vec.push_back(nums[x]);
-            }
-            for(int k=j; count<nums.size()-i;){
-                vec.push_back(nums[k]);
-                if(k==nums.size()-1){
-                    k=i;
-                }
-                else k++;
-                count++;
-            }
+    // My solution brute force   
+    
+//     void solve(vector<int> nums,set<vector<int>>& st,int i){
+//         for(int j = i;j<nums.size();j++){
+//             vector<int> vec;
+//             int count = 0;
+//             for(int x=0;x<i;x++){
+//                 vec.push_back(nums[x]);
+//             }
+//             for(int k=j; count<nums.size()-i;){
+//                 vec.push_back(nums[k]);
+//                 if(k==nums.size()-1){
+//                     k=i;
+//                 }
+//                 else k++;
+//                 count++;
+//             }
             
-            st.insert(vec);
+//             st.insert(vec);
             
-             solve(vec,st,i+1);
-        }
+//              solve(vec,st,i+1);
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         set<vector<int>> st;
+//         solve(nums,st,0);
+//         for(auto x:st){
+//             ans.push_back(x);
+//         }
+//         return ans;
+//     }
+    
+    // better solution 
+vector<vector<int> > permuteUnique(vector<int> &num) {
+    vector<vector<int>> v;
+    vector<int> r;
+    map<int, int> map;
+    for (int i : num)
+    {
+        if (map.find(i) == map.end()) map[i] = 0;
+        map[i]++;
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        set<vector<int>> st;
-        solve(nums,st,0);
-        for(auto x:st){
-            ans.push_back(x);
-        }
-        return ans;
+    permuteUnique(v, r, map, num.size());
+    return v;
+}
+
+void permuteUnique(vector<vector<int>> &v, vector<int> &r, map<int, int> &map, int n)
+{
+    if (n <= 0)
+    {
+        v.push_back(r);
+        return;
     }
+    for (auto &p : map)
+    {
+        if (p.second <= 0) continue;
+        p.second--;
+        r.push_back(p.first);
+        permuteUnique(v, r, map, n - 1);
+        r.pop_back();
+        p.second++;
+    }
+}
 };
