db8a5884a6ebccf48d58bb38927e645de6945640
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-10-21 05:05:36 +0530

    Time: 1531 ms (5.01%), Space: 136.7 MB (5.00%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+       
+    void solve(vector<int> nums,set<vector<int>>& st,int i){
+        for(int j = i;j<nums.size();j++){
+            vector<int> vec;
+            int count = 0;
+            for(int x=0;x<i;x++){
+                vec.push_back(nums[x]);
+            }
+            for(int k=j; count<nums.size()-i;){
+                vec.push_back(nums[k]);
+                if(k==nums.size()-1){
+                    k=i;
+                }
+                else k++;
+                count++;
+            }
+            st.insert(vec);
+            solve(vec,st,i+1);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> st;
+        solve(nums,st,0);
+        for(auto x:st){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
