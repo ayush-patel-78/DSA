1531c91d106d3922661bc44d130b817d80c1ae4a
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-10-22 14:04:45 +0530

    Time: 41 ms (46.61%), Space: 8.3 MB (29.52%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> mpp;
+        unordered_map<char,int> mpp2;
+        int required = 0;
+        for(int i=0;i<t.size();i++){
+            mpp[t[i]]++;
+            mpp2[t[i]]++;
+            required++;
+        }
+        vector<char> curr;
+        int ans_size = INT_MAX;
+        int j=0;
+        int i=0;
+        int formed = 0;
+        int ans_i = 0;
+        int ans_j = 0;
+        bool check = false;
+        while(j<s.size()){
+            curr.push_back(s[j]);
+            if(mpp.find(s[j]) != mpp.end()){
+                mpp[s[j]]--;
+                if(mpp[s[j]]>=0) formed++;
+            }
+            while(formed == required){
+                if(j-i+1 < ans_size){
+                    ans_size = j-i+1;
+                    ans_i = i;
+                    ans_j = j;
+                    check = true;
+                }
+                if(mpp.find(s[i]) != mpp.end()){
+                    mpp[s[i]]++;
+                    if(mpp[s[i]]>0) formed--;
+                }
+                i++;
+            }
+            j++;
+        }
+        string ans = "";
+        if(check){
+            for(int x = ans_i;x<=ans_j;x++){
+               ans += s[x];
+            }
+        }
+        
+        return ans;
+    }
+};
