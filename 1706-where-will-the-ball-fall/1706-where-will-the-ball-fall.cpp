17d28a3039ea725021252002d589ef843fb570d3
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-11-01 17:34:01 +0530

    Time: 46 ms (68.26%), Space: 14.5 MB (5.40%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,int i,int j){
+    int solve(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& dp){
         if(i==grid.size()){
            return j;
-           
         }
+        if(dp[i][j] != -2) return dp[i][j];
         int x = grid[i][j];
         if(j==grid[0].size()-1 && x==1 ) return -1;
         if(j==0 && x==-1) return -1;
         int ans = -1;
         if(x==1 && grid[i][j+1]==1){
-            ans = solve(grid,i+1,j+1);
+            ans = solve(grid,i+1,j+1,dp);
         }
         else if(x==-1 && grid[i][j-1]==-1){
-            ans = solve(grid,i+1,j-1);
+            ans = solve(grid,i+1,j-1,dp);
         }
-        return ans;
+        return dp[i][j] = ans;
         
     }
     vector<int> findBall(vector<vector<int>>& grid) {
         vector<int> ans;
+        vector<vector<int>> dp(grid.size()+1,vector<int>(grid[0].size()+1,-2));
         for(int i=0;i<grid[0].size();i++){
-            int x = solve(grid,0,i);
+            int x = solve(grid,0,i,dp);
             ans.push_back(x);
             
         }
         return ans;
     }
 };
-
-// [[1,1,1,-1,-1],
-//  [1,1,1,-1,-1],
-//  [-1,-1,-1,1,1],
-//  [1,1,1,1,-1],
-//  [-1,-1,-1,-1,-1]]
